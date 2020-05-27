cc0505b07e602cad795696f55979dc933f14c082
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-27 19:34:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
+countdown = 10
+  while countdown > 0
+    puts "'#{countdown} SECOND(S)!'"
+    countdown -= 1
+    sleep(1)
+  end
+puts "HAPPY NEW YEAR!"
+
 def countdown
-  countdown = 10
-    while countdown > 0
-      puts "'#{countdown} SECOND(S)!'"
-      countdown -= 1
-      sleep(1)
-    end
-  puts "HAPPY NEW YEAR!"
 end
